dda8051ea4c8df1402db865bd873298a1ae37712
karanv1r <43957231+karanv1r@users.noreply.github.com>
2020-11-13 14:56:23 +0530

    Add files via upload

---
+++ TOPSIS-KARANBIR-101803235-0.1/TOPSIS-KARANBIR-101803235-0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+ 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+ 
+setuptools.setup(
+  name = 'TOPSIS BY KARAN',
+  author = 'KARANBIR SINGH',                    
+  keywords = ['TOPSIS'],  
+  install_requires=['pandas'],
+  packages=setuptools.find_packages(),   
+  version = '0.1',      
+  license='TIETCOE',        
+  description = 'PACKAGE DESIGNED FOR RANKING THE DATASET BY THEIR TOPSIS SCORE',   
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: TIETCOE License',  
+    'Programming Language :: Python :: 3',     
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
